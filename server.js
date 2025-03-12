9de63c79cfa5ff1c0d47aaad5c68942cedaa3550
anandadagit <anand@ada.ac.uk>
2025-03-12 15:22:05 +0000

    Fix port binding for Render deployment

--- server.js
+++ server.js
@@ -50,6 +50,6 @@ app.post('/posts', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
